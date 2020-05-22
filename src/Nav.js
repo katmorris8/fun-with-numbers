916dbdfde5b53bc35d4e81a2000dfc242c771116
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-21 21:23:49 -0400

    created a nav component that returns the three nav buttons

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <div>
+      <button>trivia</button>
+      <button>math</button>
+      <button>date</button>
+    </div>
+  )
+}
+
+export default Nav;

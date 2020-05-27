8c7360b15960277ca045187ce464af3a30a0f902
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-26 20:15:59 -0400

    formatted code and simplified app by removing the nav

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
-import Nav from './Nav';
+// import Nav from './Nav';
+import Fact from './Fact';
+import Button from './Button';
 
-function App() {
-  return (
-    <div className="">
-      <h1>fun facts about numbers</h1>
-      <Nav />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state ={
+      type: 'random',
+      fact: 'test',
+    }
+  }
+  render() {
+    return (
+      <div className="">
+        <h1>fun facts about numbers</h1>
+        {/* <Nav />  */}
+        <Button name={this.state.type} />
+        <Fact fact={this.state.fact} />
+      </div>
+    );
+  }
 }
 
 export default App;

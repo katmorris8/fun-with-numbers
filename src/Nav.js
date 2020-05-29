499b95217619fa5bc6ab135b4c83269b58eeea09
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-05-28 20:27:19 -0400

    added a try catch block which renders an error message to the user when a fact can't be fetched. also added styling to the header

--- src/Nav.js
+++ src/Nav.js
@@ -3,7 +3,7 @@ import Button from './Button';
 
 const Nav = ({ names, click}) => {
   return (
-    <div>
+    <div className="flex justify-center">
       {
         names.map(name => {
           return(

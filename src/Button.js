6556e56bbabffa5e46df8936e892e9166ff7e7cf
katmorris8 <kathryn.leigh.morris@gmail.com>
2020-06-02 21:01:34 -0400

    reformatted the files

--- src/Button.js
+++ src/Button.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './Button.css'
 
 const Button = props => {
-  return <button className="nav-button avenir br3" onClick={props.click} value={props.name}>{props.name}</button>
+  return (
+    <button 
+      className="nav-button avenir br3" 
+      onClick={props.click} 
+      value={props.name}>
+        {props.name}
+    </button>
+  )
 }
 
 export default Button;
--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Button from './Button';
 
-const Nav = ({ names, click}) => {
+const Nav = ({ names, click }) => {
   return (
     <div className="flex justify-center">
       {

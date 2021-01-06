8faea91a07244f8693e01e0d09aca31330d93c41
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-06 08:06:27 -0500

    use custom hook for api fetch

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,10 +1,18 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export const useFetch = url => {
   const [state, setState] = useState({
     data: null,
     status: 'idle'
   })
+
+  useEffect(() => {
+    setState({data: null, status: 'pending'})
+    fetch(url)
+      .then(data => data.text())
+      .then(fact => setState({data: fact, status: 'resolved'}))
+
+  }, [url])
 
   return state
 }

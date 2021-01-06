cc4ae06b4cac728af2ce5b5a0ceef24fdde8d2d4
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-06 08:57:17 -0500

    update the click handler, implement the useFetch hook, and update the fact rendering

--- src/App.js
+++ src/App.js
@@ -3,42 +3,28 @@ import "./App.css";
 import Nav from "./Nav";
 import Fact from "./Fact";
 import "tachyons";
+import { useFetch } from "./useFetch";
 
 const App = () => {
-  const types = ["random", "trivia", "math", "date", "year"]
-  const [{number, fact}, setState] = useState({
-    number: '',
-    fact: 'Click on a button to get a fact!'
-  })
+  const types = ["random", "trivia", "math", "date", "year"];
+  const [{ currentType, message }, setState] = useState({
+    currentType: null,
+    message: "Click on a button to get a fact!",
+  });
 
-  async function handleClick(e) {
-    try {
-      if (e.target.value === "random") {
-        e.target.value = "";
-      }
-      const response = await fetch(
-        `http://numbersapi.com/random/${e.target.value}?json`
-      );
-      const json = await response.json();
-      setState({
-        number: json.number,
-        fact: json.text,
-      });
-    } catch (error) {
-      setState({
-        fact: "sorry! could not find fact. please try again later",
-      });
-      throw error;
-    }
-  };
+  function handleClick(e) {
+    setState({currentType: e.target.value})
+  }
+  
+  const {type, text, number, found, status} = useFetch(`http://numbersapi.com/random/${currentType}?json`)
 
   return (
     <div className="app">
       <h1 className="tc f1 avenir">fun facts about numbers</h1>
       <Nav names={types} click={handleClick} />
-      <Fact fact={fact} number={number} />
+      <Fact fact={found ? text : message} number={found ? number : ''} />
     </div>
   );
-}
+};
 
 export default App;

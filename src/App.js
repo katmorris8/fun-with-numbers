12b06f995a0ec3ed5ead43ba94f2c4267ed36e5c
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-06 20:06:16 -0500

    add check for the random button click

--- src/App.js
+++ src/App.js
@@ -7,22 +7,25 @@ import { useFetch } from "./useFetch";
 
 const App = () => {
   const types = ["random", "trivia", "math", "date", "year"];
-  const [{ currentType, message }, setState] = useState({
+  const [{ currentType, initialMessage }, setState] = useState({
     currentType: null,
-    message: "Click on a button to get a fact!",
+    initialMessage: "Click on a button to get a fact!",
   });
 
   function handleClick(e) {
-    setState({currentType: e.target.value === 'random' ? '' : e.target.value})
+    const { value } = e.target;
+    setState({ currentType: value === "random" ? "" : value });
   }
-  
-  const {type, text, number, found, status} = useFetch(`http://numbersapi.com/random/${currentType}?json`)
+
+  const { type, text, number, found, status } = useFetch(
+    `http://numbersapi.com/random/${currentType}?json`
+  );
 
   return (
     <div className="app">
       <h1 className="tc f1 avenir">fun facts about numbers</h1>
       <Nav names={types} click={handleClick} />
-      <Fact fact={found ? text : message} number={found ? number : ''} />
+      <Fact fact={type ? text : initialMessage} number={found ? number : ""} />
     </div>
   );
 };

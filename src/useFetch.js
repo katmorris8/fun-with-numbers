dc0b1a8e68fd33be0b32e6c3c3343dac90fff252
katmorris8 <kathryn.leigh.morris@gmail.com>
2021-01-06 20:03:51 -0500

    add handling for rejected promise

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,27 +1,38 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-export const useFetch = url => {
+export const useFetch = (url) => {
   const [state, setState] = useState({
     type: null,
     text: null,
     number: null,
     found: null,
-    status: 'idle'
-  })
+    status: "idle",
+  });
 
   useEffect(() => {
-    setState({data: null, status: 'pending'})
+    setState({ data: null, status: "pending" });
     fetch(url)
-      .then(data => data.json())
-      .then(fact => setState({
-        type: fact.type,
-        text: fact.text,
-        number: fact.number,
-        found: fact.found,
-        status: 'resolved'
-      }))
+      .then((data) => data.json())
+      .then((fact) => {
+        if (fact.text.toLowerCase().includes("invalid type")) {
+          setState({
+            status: "rejected",
+            found: false,
+            text: "There was an error trying to receive this fact. Try again!",
+          });
+        } else {
+          setState({
+            type: fact.type,
+            text: fact.text,
+            number: fact.number,
+            found: fact.found,
+            status: "resolved",
+          });
+        }
+      });
+  }, [url]);
 
-  }, [url])
+  console.log(state.found, state.status);
 
-  return state
-}
+  return state;
+};
